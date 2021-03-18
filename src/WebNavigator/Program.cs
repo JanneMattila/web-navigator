54a1ff4e35eddfb0a61f67826a43580d1de759cd
Janne Mattila <janne.mattila@hotmail.fi>
2021-03-18 12:44:25 +0200

    Add project placeholder

---
+++ src/WebNavigator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebNavigator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

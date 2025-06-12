6b5f77f701fd6e4ae327e2aa2d5186bfd864d9c0
Janne Mattila <janne.mattila@hotmail.fi>
2025-06-12 15:21:31 +0300

    Add totals row to statistics

--- src/WebNavigatorStatistics/wwwroot/js/site.js
+++ src/WebNavigatorStatistics/wwwroot/js/site.js
@@ -58,9 +58,48 @@ connection.on("UpdateStats", stats => {
         statsTableElement.deleteRow(-1);
     }
 
+    // Totals accumulators
+    let total2XX = 0;
+    let total3XX = 0;
+    let total4XX = 0;
+    let total5XX = 0;
+
     for (let i = 0; i < stats.length; i++) {
         addTableRow(statsTableElement, stats[i]);
+        // Sum up all status codes for totals
+        for (let j = 0; j < stats[i].httpStats.length; j++) {
+            let httpStat = stats[i].httpStats[j];
+            total2XX += Number(httpStat.statusCode2XX) || 0;
+            total3XX += Number(httpStat.statusCode3XX) || 0;
+            total4XX += Number(httpStat.statusCode4XX) || 0;
+            total5XX += Number(httpStat.statusCode5XX) || 0;
+        }
     }
+
+    // Add Totals row at the end
+    let totalsRow = statsTableElement.insertRow(-1);
+
+    let totalsLabelCell = totalsRow.insertCell(0);
+    totalsLabelCell.colSpan = 3;
+    totalsLabelCell.style.fontWeight = "bold";
+    totalsLabelCell.appendChild(document.createTextNode("Totals"));
+
+    let totals2XXCell = totalsRow.insertCell(1);
+    totals2XXCell.appendChild(document.createTextNode(total2XX));
+
+    let totals3XXCell = totalsRow.insertCell(2);
+    totals3XXCell.appendChild(document.createTextNode(total3XX));
+
+    let totals4XXCell = totalsRow.insertCell(3);
+    totals4XXCell.appendChild(document.createTextNode(total4XX));
+
+    let totals5XXCell = totalsRow.insertCell(4);
+    totals5XXCell.appendChild(document.createTextNode(total5XX));
+
+    let totalSum = total2XX + total3XX + total4XX + total5XX;
+    let totalsSumCell = totalsRow.insertCell(5);
+    totalsSumCell.style.fontWeight = "bold";
+    totalsSumCell.appendChild(document.createTextNode(totalSum));
 
     let totalClients = 0;
     let machineNames = new Set();
